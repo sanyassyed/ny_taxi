a5a8a382a9954b8f7d0e202c929fe61b30a78d49
Sanya Syed <36104388+sanyassyed@users.noreply.github.com>
2024-01-29 01:16:15 +0000

    CICD:Updated pipeline

--- docs/commands.md
+++ docs/commands.md
@@ -18,3 +18,32 @@ postgres:13
 * `ls -lah` -> to check the permissions for the folders
 * `sudo chown codespace:codespace db_data` -> to change the owner of the folder db_data
 * `jupyter notebook` -> to run jupyter notebook
+
+
+## SQL
+SELECT COUNT(1)
+FROM green_taxi_data
+WHERE lpep_pickup_datetime LIKE '2019-09-18%' AND lpep_dropoff_datetime LIKE '2019-09-18%'
+
+SELECT lpep_pickup_datetime, trip_distance
+FROM green_taxi_data
+ORDER BY 2 DESC
+LIMIT 1
+
+SELECT 'z.Borough', SUM(CAST('gt.total_amount' AS DOUBLE PRECISION))
+FROM green_taxi_data as gt, zone as z
+WHERE 'gt.PULocationID' = 'z.LocationID' AND CAST('gt.lpep_pickup_datetime' AS DATE) = '2019-09-18'
+GROUP BY 1
+ORDER BY 2 DESC
+
+SELECT 'PULocationID', SUM ('total_amount'::DOUBLE PRECISION) as total
+FROM green_taxi_data as gt
+WHERE'gt.lpep_pickup_datetime' = '2019-09-18'
+GROUP BY 1
+ORDER BY 2 DESC
+
+SELECT 'Borough', SUM(total_amount)
+FROM green_taxi_data, zone
+WHERE lpep_pickup_datetime LIKE '2019-09-18%' AND 'PULocationID' = 'LocationID'
+GROUP BY 'Borough'
+ORDER BY 2 DESC
--- pipeline_zone.py
+++ pipeline_zone.py
@@ -28,26 +28,7 @@ def con_close(engine):
     return None
 
 
-def extract(url) -> str:
-    """
-    Extracts the parquet file from the url and writes to the disk
-    Args:
-        url (str): the url the source data
-    Returns:
-        f_loc (str): the location where the parquet file is written as a cvs file
-    """
-    data_dir = 'data'
-    f_name = (url.split('.parquet')[0].split('/')[-1]) + '.csv'
-    f_loc = os.path.join(data_dir, f_name)
-    # reading parquet file from url as df
-    df_p = pd.read_parquet(url)
-    print(f'Parquet file downloaded from {url}')
-    # writing df to disk as csv for future reading by chunking and iteration
-    df_p.to_csv(f_loc, index=False)
-    print(f'Parquet file written to disk as CSV at {f_loc}')
-    return f_loc
-
-def transform(df):
+def load(engine, url, taxi_color):
     """
     Extracts sheets from Gdrive by specifying the url in the pandas read_excel function
     Args:
@@ -57,48 +38,21 @@ def transform(df):
         df_i (pandas.dataframe): Pandas df containing inventory data
         df_p (pandas.dataframe): Pandas df containing production data
     """
-    # Converting the datatype pf columns total_amount, passenger_count, RatecodeID to appropriate type
-    df.passenger_count = df['passenger_count'].fillna(0).astype('int')
-    df.RatecodeID = df['RatecodeID'].fillna(0).astype('int')
-    df.total_amount = df['total_amount'].fillna(0).astype('int')
-    return df
-
-def load(engine, file_loc, taxi_color):
-    """
-    Extracts sheets from Gdrive by specifying the url in the pandas read_excel function
-    Args:
-        url (str): the url of the spreadsheet which is the data source
-        username (list): names of the sheets to be extracted
-    Returns:
-        df_i (pandas.dataframe): Pandas df containing inventory data
-        df_p (pandas.dataframe): Pandas df containing production data
-    """
-    df_iter = pd.read_csv(file_loc, iterator=True, chunksize=100000, low_memory=False)
+    df_iter = pd.read_csv(url, iterator=True, chunksize=100000, low_memory=False)
     df = next(df_iter)
-    table_name = f'{taxi_color}_taxi_data'
+    table_name = taxi_color
     # inserting only column headers
     df.head(0).to_sql(name=table_name, con=engine, if_exists='replace')
     while df is not None:
         s_time = time()
-        df = transform(df)
         df.to_sql(name=table_name, con=engine, if_exists='append')
         e_time = time()
         print(f'Inserted chunk in {e_time-s_time:.2f} seconds')
         df = next(df_iter, None)
     return None
 
-def del_download(f_loc):
-    """
-    Deletes the csv written on disk
-    Args:
-        file_loc (str): file location on disk
-    Return:
-        None
-    """
-    os.remove(f_loc)
-    print(f'File deletion from {f_loc} complete')
 
-def main(taxi_color='green', year=2019, month=9)->None:
+def main(taxi_color='zone')->None:
     """
     Main function that calls the ETL functions
     Args:
@@ -109,15 +63,12 @@ def main(taxi_color='green', year=2019, month=9)->None:
         None
     """
     host, username, pwd, port, db_name = 'localhost', 'codespace', 'root', 5432, 'ny_taxi'
-    url =f'https://d37ci6vzurychx.cloudfront.net/trip-data/{taxi_color}_tripdata_{year}-{month:02d}.parquet'
-    f_loc = extract(url)
-    print('Data Extraction Complete')
+    url =f'https://d37ci6vzurychx.cloudfront.net/misc/taxi+_{taxi_color}_lookup.csv'
     engine = con_open(host, username, pwd, port, db_name)
     if engine is not None:
-        load(engine, f_loc, taxi_color)
+        load(engine, url, taxi_color)
         print("Data Loading to DB Complete")
         con_close(engine)
-        del_download(f_loc)
 
 if __name__=="__main__":
     main()
